acfeec8e48fbdef2395f8d2e70eed20c51ebf81c
test0071030 <47777484+test0071030@users.noreply.github.com>
2019-04-15 20:34:16 +0800

    Update app.js

--- app.js
+++ app.js
@@ -10,7 +10,6 @@ let display = true
 			$("#content .quote").toggle("slow")
 			$(".frame").not(this).toggle("slow")
 			$(this).toggleClass( "showing", 1000)
-			current=this
 			if($(this).is("#CHUEH")){
 				display?$("#CHUEH img").attr("src","https://i.imgur.com/adK2Tmu.png"):$("#CHUEH img").attr("src","https://i.imgur.com/Fq8AHjI.png");
 				$("#CHUEH img").toggleClass( "showing", 1000);
@@ -41,40 +40,41 @@ let display = true
 				$("#CHEN img").toggleClass( "showing", 1000);
 				display = !display
 			}
+			tempsave=this
 			
 			$(".back").click(function(){
 				$("#content p").toggle("slow")
 				$("#content .title").toggle("slow")
 				$("#content .back").toggle("slow")
 				$("#content .quote").toggle("slow")
-				$(".frame").not(current).toggle("slow")
-				$(current).toggleClass( "showing", 1000)
-				if($(current).is("#CHUEH")){
+				$(".frame").not(tempsave).toggle("slow")
+				$(tempsave).toggleClass( "showing", 1000)
+				if($(tempsave).is("#CHUEH")){
 					display?$("#CHUEH img").attr("src","https://i.imgur.com/adK2Tmu.png"):$("#CHUEH img").attr("src","https://i.imgur.com/Fq8AHjI.png");
 					$("#CHUEH img").toggleClass( "showing", 1000);
 					display = !display
 				}$
-				if($(current).is("#KE")){
+				if($(tempsave).is("#KE")){
 					display?$("#KE img").attr("src","https://i.imgur.com/ZRgJHOl.png"):$("#KE img").attr("src","https://i.imgur.com/eCyrqtM.png")
 					$("#KE img").toggleClass( "showing", 1000);
 					display = !display
 				}
-				if($(current).is("#SU")){
+				if($(tempsave).is("#SU")){
 					display?$("#SU img").attr("src","https://i.imgur.com/Bv17oaZ.png"):$("#SU img").attr("src","https://i.imgur.com/f2svmBF.png")
 					$("#SU img").toggleClass( "showing", 1000);
 					display = !display
 				}
-				if($(current).is("#LIAO")){
+				if($(tempsave).is("#LIAO")){
 					display?$("#LIAO img").attr("src","https://i.imgur.com/zRc62vI.png"):$("#LIAO img").attr("src","https://i.imgur.com/mwhHx7L.png")
 					$("#LIAO img").toggleClass( "showing", 1000);
 					display = !display
 				}
-				if($(current).is("#WU")){
+				if($(tempsave).is("#WU")){
 					display?$("#WU img").attr("src",""):$("#WU img").attr("src","")
 					$("#WU img").toggleClass( "showing", 1000);
 					display = !display
 				}
-				if($(current).is("#CHEN")){
+				if($(tempsave).is("#CHEN")){
 					display?$("#CHEN img").attr("src","https://i.imgur.com/pwQW97u.png"):$("#CHEN img").attr("src","https://i.imgur.com/9s5Mq5w.png")
 					$("#CHEN img").toggleClass( "showing", 1000);
 					display = !display
